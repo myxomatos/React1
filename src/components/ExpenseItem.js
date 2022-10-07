c2693e9f5588b26d7c786fc748be1d0feb617728
Aldo Sanchez <myxomatos46@gmail.com>
2022-10-07 17:44:12 -0500

    Add ExpenseItem.css to ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+
+import "./ExpenseItem.css";
 
 const ExpenseItem = () => {
   return (
-    <h2>Expense item!</h2>
-  )
-}
+    <div className="expense-item">
+      <div>October 8 2022</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">$294,60</div>
+      </div>
+    </div>
+  );
+};
 
-export default ExpenseItem
+export default ExpenseItem;
