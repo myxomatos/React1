de8897dff8b55698fcab252633b217e58fef8ce2
Aldo Sanchez <myxomatos46@gmail.com>
2022-10-07 21:02:23 -0500

    Sec3class37 adding expenses array on App.js and passed as props to ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,13 +2,13 @@ import React from "react";
 
 import "./ExpenseItem.css";
 
-const ExpenseItem = () => {
+const ExpenseItem = (props) => {
   return (
     <div className="expense-item">
-      <div>October 8 2022</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294,60</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
