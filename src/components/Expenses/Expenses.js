7db2ac5f15768793c1ce91921bdbcae31a656cdf
Aldo Sanchez <myxomatos46@gmail.com>
2022-11-07 19:32:12 -0600

    Funtional filter and ExpensesList js/css added

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,16 +2,19 @@ import React, { useState } from "react";
 
 import Card from "../UI/Card";
 import ExpenseFilter from "./ExpenseFilter";
-import ExpenseItem from "./ExpenseItem";
-
+import ExpensesList from "./ExpensesList";
 import "./Expenses.css";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
 
   const filterChangeHandler = (selectedYear) => {
-    console.log(selectedYear);
+    setFilteredYear(selectedYear);
   };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
 
   return (
     <div>
@@ -20,26 +23,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        <ExpenseItem
-          date={props.items[0].date}
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-        />
-        <ExpenseItem
-          date={props.items[1].date}
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-        />
-        <ExpenseItem
-          date={props.items[2].date}
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-        />
-        <ExpenseItem
-          date={props.items[3].date}
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-        />
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
